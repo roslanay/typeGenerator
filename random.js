61706653346321ca089600de6d618fdba87d5e78
Roslana Yotova <roslanayotova@gmail.com>
2019-09-20 00:11:29 +0300

    Add files via upload

---
+++ random.js
@@ -0,0 +1,14 @@
+$('#test').click(function() {
+    var docHeight = $(document).height(),
+        docWidth = $(document).width(),
+        $div = $('#test'),
+        divWidth = $div.width(),
+        divHeight = $div.height(),
+        heightMax = docHeight - divHeight,
+        widthMax = docWidth - divWidth;
+    
+    $div.css({
+        left: Math.floor( Math.random() * widthMax ),
+        top: Math.floor( Math.random() * heightMax )
+    });
+});
